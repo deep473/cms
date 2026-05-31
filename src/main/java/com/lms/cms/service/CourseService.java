a42100506803b94ef4e0de137f65055c610e88f1
deep473 <deepztanshu@gmail.com>
2026-05-31 16:39:13 +0530

    video-09: implemented DTO layer

--- src/main/java/com/lms/cms/service/CourseService.java
+++ src/main/java/com/lms/cms/service/CourseService.java
@@ -1,12 +1,13 @@
 package com.lms.cms.service;
 
 import com.lms.cms.dto.CourseRequestDTO;
-import com.lms.cms.dto.CourseRequestDtoTraditional;
+import com.lms.cms.dto.CourseResponseDTO;
 import com.lms.cms.entity.Course;
 import com.lms.cms.repository.CourseRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CourseService {
@@ -17,8 +18,54 @@ public class CourseService {
         this.courseRepository = courseRepository;
     }
 
-    public Course createCourse(CourseRequestDTO course){
-        return courseRepository.save(mapToEntity(course));
+    public CourseResponseDTO createCourse(CourseRequestDTO course){
+        Course createdCourse = courseRepository.save(mapToEntity(course));
+        return mapToResponseDTO(createdCourse);
+    }
+
+    public CourseResponseDTO getCourseById(Long id) {
+        Course course = courseRepository.findById(id).get();
+        return mapToResponseDTO((course));
+    }
+
+    public List<CourseResponseDTO> getAllCourses() {
+        List<Course> courseList = courseRepository.findAll();
+
+        return courseList.stream()
+                .map(this::mapToResponseDTO)
+                .toList();
+    }
+
+    public CourseResponseDTO updateCoursePartially(Long id, CourseRequestDTO updatedCourse) {
+        Course existingCourse = courseRepository.findById(id).get();
+
+        if(updatedCourse.name() != null)
+            existingCourse.setName(updatedCourse.name());
+
+        if(updatedCourse.description() != null)
+            existingCourse.setDescription(updatedCourse.description());
+
+        if(updatedCourse.price() != null)
+            existingCourse.setPrice(updatedCourse.price());
+
+        Course savedCourse = courseRepository.save(existingCourse);
+        return mapToResponseDTO(savedCourse);
+    }
+
+    public CourseResponseDTO updateCourseCompletely(Long id, CourseRequestDTO updatedCourse) {
+        Course existingCourse = courseRepository.findById(id).get();
+
+        existingCourse.setName(updatedCourse.name());
+        existingCourse.setDescription(updatedCourse.description());
+        existingCourse.setPrice(updatedCourse.price());
+
+        Course savedCourse = courseRepository.save(existingCourse);
+        return mapToResponseDTO(savedCourse);
+    }
+
+    public String deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+        return "Course deleted successfully!";
     }
 
     private Course mapToEntity(CourseRequestDTO dto) {
@@ -29,41 +76,13 @@ public class CourseService {
         return course;
     }
 
-    public Course getCourseById(Long id) {
-        return courseRepository.findById(id).get();
+    private CourseResponseDTO mapToResponseDTO(Course course) {
+        return new CourseResponseDTO(
+                course.getId(),
+                course.getName(),
+                course.getDescription(),
+                course.getPrice()
+        );
     }
 
-    public List<Course> getAllCourses() {
-        return courseRepository.findAll();
-    }
-
-    public Course updateCoursePartially(Long id, CourseRequestDtoTraditional updatedCourse) {
-        Course existingCourse = courseRepository.findById(id).get();
-
-        if(updatedCourse.getName() != null)
-            existingCourse.setName(updatedCourse.getName());
-
-        if(updatedCourse.getDescription() != null)
-            existingCourse.setDescription(updatedCourse.getDescription());
-
-        if(updatedCourse.getPrice() != null)
-            existingCourse.setPrice(updatedCourse.getPrice());
-
-        return courseRepository.save(existingCourse);
-    }
-
-    public Course updateCourseCompletely(Long id, CourseRequestDtoTraditional updatedCourse) {
-        Course existingCourse = courseRepository.findById(id).get();
-
-        existingCourse.setName(updatedCourse.getName());
-        existingCourse.setDescription(updatedCourse.getDescription());
-        existingCourse.setPrice(updatedCourse.getPrice());
-
-        return courseRepository.save(existingCourse);
-    }
-
-    public String deleteCourse(Long id) {
-        courseRepository.deleteById(id);
-        return "Course deleted successfully!";
-    }
 }
